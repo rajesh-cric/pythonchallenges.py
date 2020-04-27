0390fbeb87f6f5ce20593fccafed6c36a18e1815
rajesh-cric <60807489+rajesh-cric@users.noreply.github.com>
2020-04-27 14:13:09 +0530

    Update code5.py

--- code5.py
+++ code5.py
@@ -1,4 +1,5 @@
-
+print("ROOM ALLOTMENT ON THE BASIS OF YOUR FIRST LETTER OF NAME:")
+name=input("enter your first name: ")
 if name1[0].capitalize()=="A" or name1[0].capitalize()=="B":
     print(name1.capitalize(),'should be in room AB')
 elif name1[0].capitalize()=="C":

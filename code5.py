851ebd09ff253da36bca2505891ba72cf27c0b13
rajesh-cric <60807489+rajesh-cric@users.noreply.github.com>
2020-04-27 14:03:29 +0530

    Add files via upload

---
+++ code5.py
@@ -0,0 +1,12 @@
+
+if name1[0].capitalize()=="A" or name1[0].capitalize()=="B":
+    print(name1.capitalize(),'should be in room AB')
+elif name1[0].capitalize()=="C":
+    print(name1.capitalize(),'should be in room CD')
+else:
+    name2=input("enter your last name: ")
+    if name2[0].capitalize()=="Z":
+        print(name1.capitalize(),name2.capitalize(),'should be in room Z')
+    else:
+        print(name1.capitalize(),name2.capitalize(),'should be in room OTHER')
+    

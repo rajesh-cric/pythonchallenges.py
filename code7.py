c6f6869617c585d0ccd1a668746911ea4195dcc0
rajesh-cric <60807489+rajesh-cric@users.noreply.github.com>
2020-04-27 15:02:38 +0530

    Add files via upload

---
+++ code7.py
@@ -0,0 +1,7 @@
+
+print("\n Why won't this line of code print: ")
+print("\n this line fails too ")
+print("\n I think I know how to fix this one ")
+print()
+name=input("please tell me your name: ")
+print("NAME: ",name.capitalize())
